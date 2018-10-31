346e2c15cee33de4b5c072ac81ee75293226b277
Maciek Bulski <fragmast3r@gmail.com>
2018-10-31 18:09:06 +0000

    deleted calendars and logo changed

--- js/rooms.js
+++ js/rooms.js
@@ -2,56 +2,37 @@ $(document).ready(function() {
     $("#btn_r_1").click(function() {
         $(".room").css('display', 'none');
         $("#room_1").css('display', 'flex');
-		$(".check_button").css('display', 'none');
-		$("#check_button_1").css('display', 'block');
-		
     });
     $("#btn_r_2").click(function() {
         $(".room").css('display', 'none');
         $("#room_2").css('display', 'flex');
-		$(".check_button").css('display', 'none');
-		$("#check_button_2").css('display', 'block');
     });
     $("#btn_r_5").click(function() {
         $(".room").css('display', 'none');
         $("#room_5").css('display', 'flex');
-		$(".check_button").css('display', 'none');
-		$("#check_button_5").css('display', 'block');
     });
 	$("#btn_r_6").click(function() {
         $(".room").css('display', 'none');
         $("#room_6").css('display', 'flex');
-		$(".check_button").css('display', 'none');
-		$("#check_button_6").css('display', 'block');
     });
 	$("#btn_r_7").click(function() {
         $(".room").css('display', 'none');
         $("#room_7").css('display', 'flex');
-		$(".check_button").css('display', 'none');
-		$("#check_button_7").css('display', 'block');
     });
 	$("#btn_r_8").click(function() {
         $(".room").css('display', 'none');
         $("#room_8").css('display', 'flex');
-		$(".check_button").css('display', 'none');
-		$("#check_button_8").css('display', 'block');
     });
 	$("#btn_r_9").click(function() {
         $(".room").css('display', 'none');
         $("#room_9").css('display', 'flex');
-		$(".check_button").css('display', 'none');
-		$("#check_button_9").css('display', 'block');
     });
 	$("#btn_r_10").click(function() {
         $(".room").css('display', 'none');
         $("#room_10").css('display', 'flex');
-		$(".check_button").css('display', 'none');
-		$("#check_button_10").css('display', 'block');
     });
 	$("#btn_r_11").click(function() {
         $(".room").css('display', 'none');
         $("#room_11").css('display', 'flex');
-		$(".check_button").css('display', 'none');
-		$("#check_button_11").css('display', 'block');
     });
 });
